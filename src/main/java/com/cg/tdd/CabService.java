db1f32f17bcb940fef6b7a6863240f65a80165c6
Aditya Viren <adityaviren@gmail.com>
2020-10-26 15:11:32 +0530

    Added calculate fare method

--- src/main/java/com/cg/tdd/CabService.java
+++ src/main/java/com/cg/tdd/CabService.java
@@ -3,6 +3,10 @@ package com.cg.tdd;
 public class CabService {
 
     public long fare(int minutes, int kilometers){
-        return 0;
+        long fare = minutes + kilometers*10;
+        if(fare<5)
+            return 5;
+        else
+            return fare;
     }
 }
--- src/test/java/com/cg/tdd/TDD.java
+++ src/test/java/com/cg/tdd/TDD.java
@@ -1,10 +1,14 @@
 package com.cg.tdd;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TDD {
 
     @Test
-    public void name() {
+    public void givenMinutesKilometers_shouldReturnFare() {
+        CabService cabService = new CabService();
+        long fare = cabService.fare(1,0);
+        Assert.assertEquals(5,fare);
     }
 }
